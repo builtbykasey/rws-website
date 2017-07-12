992f01acea6e404576754fa7c72d0f302bc5027a
Kasey Markham <kaseycm@gmail.com>
2017-07-12 16:25:14 -0400

    Scroll to setup for nav links

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,46 @@
-// If user scrolls, change transparent menus to default menus
+$("#home-link").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#home").offset().top
+    }, 1200);
+    return false;
+});
 
+$("#about-link").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#about").offset().top
+    }, 1200);
+    return false;
+});
+
+$("#services-link").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#services").offset().top
+    }, 1200);
+    return false;
+});
+
+$("#process-link").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#process").offset().top
+    }, 1200);
+    return false;
+});
+
+$("#team-link").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#team").offset().top
+    }, 1200);
+    return false;
+});
+
+$("#contact-link").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#contact").offset().top
+    }, 1200);
+    return false;
+});
+
+// If user scrolls, change transparent menus to default menus
 var $document = $(document),
     $element = $('.navbar'),
     navbarDefault = 'navbar-default';
@@ -23,5 +64,5 @@ $document.scroll(function() {
 });
 
 $document.ready(function() {
-  
+
 });
